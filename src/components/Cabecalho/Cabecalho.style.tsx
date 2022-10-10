95589206ee619543000609b3a1acf9f5255adee8
Leo Serrão Barbosa <1340279@ifb.local>
2022-10-10 11:24:50 -0300

    Criando um componente estilizado com Material UI

---
+++ src/components/Cabecalho/Cabecalho.style.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { color } from "@mui/system";
+
+
+export const CabecalhoContainer = styled('header')`
+    color: red;
+    background-color: black;
+`;
---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,11 @@
+import { CabecalhoContainer } from "./Cabecalho.style";
+
+const Cabecalho = () => {
+    return (
+        <CabecalhoContainer>
+            Cabeçalho Home Page
+        </CabecalhoContainer>
+    )
+}
+
+export default Cabecalho;
